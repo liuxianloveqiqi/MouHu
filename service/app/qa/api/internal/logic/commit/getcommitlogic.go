cac592037380337742adb1cd021180ac3f343392
liuxian <2929504046@qq.com>
2023-06-02 17:35:33 +0800

    qa 得到所有回答，评论的api

--- service/app/qa/api/internal/logic/commit/getcommitlogic.go
+++ service/app/qa/api/internal/logic/commit/getcommitlogic.go
@@ -1,6 +1,8 @@
 package commit
 
 import (
+	"MouHu/service/app/qa/rpc/types/qa"
+	"MouHu/service/common/errorx"
 	"context"
 
 	"MouHu/service/app/qa/api/internal/svc"
@@ -25,6 +27,31 @@ func NewGetCommitLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetComm
 
 func (l *GetCommitLogic) GetCommit() (resp *types.GetCommitResp, err error) {
 	// todo: add your logic here and delete this line
+	userId, ok := l.ctx.Value("user_id").(int64)
+	if !ok {
+		return nil, errorx.NewDefaultError("user_id获取失败")
+	}
+	res, err := l.svcCtx.Rpc.GetCommit(l.ctx, &qa.GetCommitReq{
+		UserId: userId,
+	})
+	if err != nil {
+		return nil, errorx.NewDefaultError(err.Error())
+	}
+	alist := make([]types.AnswerList, 0)
+	for _, v := range res.Commit {
+		a := types.AnswerList{
+			AnswerId:   v.AnswerId,
+			UserId:     v.UserId,
+			QuestionId: v.QuestionId,
+			ParentId:   v.ParentId,
+			Content:    v.Content,
+			IsAnswer:   v.IsAnswer,
+			CreateTime: v.CreateTime.AsTime().Format("2006-01-02 15:04:05"),
+			UpdateTime: v.UpdateTime.AsTime().Format("2006-01-02 15:04:05"),
+			DeleteTime: v.DeleteTime.AsTime().Format("2006-01-02 15:04:05"),
+		}
+		alist = append(alist, a)
+	}
+	return &types.GetCommitResp{Commit: alist}, nil
 
-	return
 }
--- service/app/qa/rpc/internal/logic/getcommitlogic.go
+++ service/app/qa/rpc/internal/logic/getcommitlogic.go
@@ -1,7 +1,10 @@
 package logic
 
 import (
+	"MouHu/service/app/qa/model"
 	"context"
+	"errors"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"MouHu/service/app/qa/rpc/internal/svc"
 	"MouHu/service/app/qa/rpc/types/qa"
@@ -25,6 +28,31 @@ func NewGetCommitLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetComm
 
 func (l *GetCommitLogic) GetCommit(in *qa.GetCommitReq) (*qa.GetCommitResp, error) {
 	// todo: add your logic here and delete this line
+	var answerList []model.Answer
+	if result := l.svcCtx.Mdb.Where("user_id = ? and is_answer = ?", in.UserId, 0).Find(&answerList); result.Error != nil {
+		if result.RowsAffected == 0 {
+			// 查询结果为空
+			return nil, errors.New("查询结果为空")
+		} else {
+			// 其他错误
+			return nil, result.Error
+		}
+	}
+	alist := make([]*qa.AnswerList, 0)
+	for _, v := range answerList {
+		a := &qa.AnswerList{
+			AnswerId:   v.Id,
+			UserId:     v.UserId,
+			QuestionId: v.QuestionId,
+			ParentId:   v.ParentId,
+			Content:    v.Content,
+			IsAnswer:   false,
+			CreateTime: timestamppb.New(v.CreateTime),
+			UpdateTime: timestamppb.New(v.UpdateTime),
+			DeleteTime: timestamppb.New(v.DeleteTime.Time),
+		}
+		alist = append(alist, a)
+	}
 
-	return &qa.GetCommitResp{}, nil
+	return &qa.GetCommitResp{Commit: alist}, nil
 }
